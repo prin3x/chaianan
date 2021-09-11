f94a91421a02c8c9cc54a6e38d85f844b8dc040e
Sarut <sarut1994@gmail.com>
2021-09-11 13:42:18 +0700

    newly update

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
   // const res = await fetch('https://.../posts')
   const posts = [
     { id: 1, title: 'มอบอุปกรณ์การแพทย์', image: '/assets/activ01.png' },
-    { id: 2, title: 'มอบอุปกรณ์การแพทย์', image: '/assets/activ01.png' },
-    { id: 3, title: 'มอบอุปกรณ์การแพทย์', image: '/assets/activ01.png' },
+    { id: 2, title: 'มอบอุปกรณ์การแพทย์2', image: '/assets/activ01.png' },
+    { id: 3, title: 'มอบอุปกรณ์การแพทย์3', image: '/assets/activ01.png' },
   ];
 
   return {
